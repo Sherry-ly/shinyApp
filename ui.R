d79b2b73b588888baa473719d3f341a7ce68ccee
Sherry-ly <70619311+Sherry-ly@users.noreply.github.com>
2020-09-03 15:03:47 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,15 +1,11 @@
 library(shiny)
 
-# Define UI for application that draws 9 plots
+# Define UI for application that draws a histogram
 shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("Plots of the Market Information / Noise Structures"),
-
 
         # Show a plot of the generated distribution
         mainPanel(
             h3(textOutput("caption"))
         )
     )
-))
+)
