2ec5ea02749943dec178cc8e9f4158bc1fa77049
Sherry-ly <70619311+Sherry-ly@users.noreply.github.com>
2020-09-03 14:42:06 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -6,12 +6,6 @@ shinyUI(fluidPage(
     # Application title
     titlePanel("Plots of the Market Information / Noise Structures"),
 
-    # Sidebar with a text input of ticker and feed source
-    sidebarLayout(
-        sidebarPanel(
-            textInput("feedSource", "feed source: "),
-            textInput("ticker", "ticker: ")
-        ),
 
         # Show a plot of the generated distribution
         mainPanel(
